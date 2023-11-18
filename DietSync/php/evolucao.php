29cab963a6f8c32f0787595778c5cdbf1f1b9ea8
Gabriel Scremim <gabrielscremim@edu.unifil.br>
2023-11-18 18:06:38 -0300

    gravar

--- DietSync/php/evolucao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$titulo = "Evolução";
-$page = 'evolucao';
-include '../php/includes/header.inc.php';
-include '../php/includes/menu.inc.php';
-?>
-    </body>
-
-    </html>
---
+++ classes/controller/index.cont.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class Menu
+{
+    private $pdo;
+    public function __construct($dbname, $host, $user, $senha)
+    {
+        try {
+            $this->pdo = new PDO("mysql:dbname=" . $dbname . ";host=" . $host, $user, $senha);
+        } catch (PDOException $th) {
+            echo "erro com banco de dados" . $th->getMessage();
+            exit();
+        } catch (PDOException $th) {
+            echo "erro generico: " . $th->getMessage();
+            exit();
+        }
+    }
+}
